c6c577984b3b6297bc907e15a112f4c4b9cb31c3
Daniel Komesu <danielkomesu@gmail.com>
2022-06-24 21:40:36 -0300

    🍞 Change env var

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ from system_stats_log import get_stats, write_log
 
 if __name__ == "__main__":
     filepath = Path(
-        os.getenv("SYS_STATS_LOG_FILEPATH", "C:\\tmp\\sys-stats-log.tsv"),
+        os.getenv("SYS_STATS_LOG_FILEPATH", "sys-stats-log.tsv"),
     )
     system_stats = get_stats()
     write_log(system_stats, filepath)
